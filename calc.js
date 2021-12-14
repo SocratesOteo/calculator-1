48b4b3e3e541cdce59b7044a20fba6278288e964
Paloma Oteo <oteokids@gmail.com>
2021-12-14 16:05:13 -0700

    added the rest of the calculator

--- calc.js
+++ calc.js
@@ -8,11 +8,27 @@ const reader = readline.createInterface({
 
 
 reader.question("What would you like to calculate?", function(input){
-	tokens = input.split(' ');
+	let tokens = input.split(' ');
 	
-	mathSymbol = tokens[0];
-	num1 = Number(tokens[1]);
-	num2 = Number(tokens[2]);
+    let mathSymbol = tokens[0];
+	let num1 = Number(tokens[1]);
+	let num2 = Number(tokens[2]);
+
+	//console.log('mathSymbol', mathSymbol);
+           // console.log('num1', num1);
+           // console.log('num2', num2);
+
+	if (mathSymbol === "+"){
+		console.log(`Answer is : ${(num1 + num2)}`); 
+	} else if (mathSymbol === "-"){
+		console.log(`Answer is : ${num1 - num2}`);
+	} else if(mathSymbol === "*"){
+		console.log(`Answer is ${num1 * num2}`);
+	} else if(mathSymbol === "/"){
+		console.log(`Answer is ${num1 / num2}`);
+	} else if(mathSymbol === "^"){
+		console.log(`Answer is ${math.sqrt((num1*num1) + (num2*num2))}`);
+	}
 
 
 	// This line closes the connection to the command line interface.
